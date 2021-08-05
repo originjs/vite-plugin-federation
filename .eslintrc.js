1c3676d4d39adffd1e9321dd19568ec47cdf064c
ruleeeer <me@ruleeeer.cn>
2021-08-05 15:07:26 +0800

    ci: replace alloy with vue/vite config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,32 @@
+const DOMGlobals = ['window', 'document']
+const NodeGlobals = ['module', 'require']
+
 module.exports = {
-  extends: ['alloy', 'alloy/typescript'],
-  env: {
-    // Your environments (which contains several predefined global variables)
-    //
-    // browser: true,
-    // node: true,
-    // mocha: true,
-    // jest: true,
-    // jquery: true
-  },
-  globals: {
-    // Your global variables (setting to false means it's not allowed to be reassigned)
-    //
-    // myGlobal: false
-    VitePluginFederationOptions: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module'
   },
   rules: {
-    // Customize your rules
-  },
-};
+    'no-unused-vars': [
+      'error',
+      // we are only using this rule to check for unused arguments since TS
+      // catches unused variables but not args.
+      { varsIgnorePattern: '.*', args: 'none' }
+    ],
+    // most of the codebase are expected to be env agnostic
+    'no-restricted-globals': ['error', ...DOMGlobals, ...NodeGlobals],
+    // since we target ES2015 for baseline support, we need to forbid object
+    // rest spread usage (both assign and destructure)
+    'no-restricted-syntax': [
+      'error',
+      'ObjectExpression > SpreadElement',
+      'ObjectPattern > RestElement',
+      'AwaitExpression'
+    ]
+  }
+}
