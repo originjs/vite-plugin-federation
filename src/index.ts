6866b16455d1ee7f4450e4503036ed084bc0d653
ruleeeer <me@ruleeeer.cn>
2021-08-05 09:23:02 +0800

    feat: suppress import warning and code format

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-import * as path from 'path';
-import {InputOptions, OutputBundle, OutputChunk, OutputOptions} from "rollup";
-
-const remoteEntryHelperId = 'rollup-plugin-federation/remoteEntry';
-const modulePrefix = '__ROLLUP_FEDERATION_MODULE_PREFIX__'
-const replaceMap = new Map()
+import * as path from 'path'
+import { InputOptions, OutputBundle, OutputChunk, OutputOptions } from 'rollup'
 
 export default function federation(options: VitePluginFederationOptions) {
-    const provideExposes = options.exposes as string[];
-    let moduleMap = '';
-    for (const key in provideExposes) {
-        moduleMap += `\n"${key}":()=>{return import('${modulePrefix + '${' + provideExposes[key] + '}'}')}`
-    }
-    const code =
-        `let moduleMap = {${moduleMap}}
+  const remoteEntryHelperId = 'rollup-plugin-federation/remoteEntry'
+  const modulePrefix = '__ROLLUP_FEDERATION_MODULE_PREFIX__'
+  const replaceMap = new Map()
+  const moduleNames: string[] = []
+  const provideExposes = options.exposes as string[]
+  let moduleMap = ''
+  for (const key in provideExposes) {
+    let moduleName = `${modulePrefix + '${' + provideExposes[key] + '}'}`
+    moduleNames.push(moduleName)
+    moduleMap += `\n"${key}":()=>{return import('${moduleName}')},`
+  }
+  const code = `let moduleMap = {${moduleMap}}
 export const get =(module, getScope) => {
     return moduleMap[module]();
 };
@@ -20,69 +21,78 @@ export const init =(shareScope, initScope) => {
     console.log('init')
 };`
 
-    return {
-        name: 'federation',
+  return {
+    name: 'federation',
 
-        options(_options: InputOptions) {
-            // Split expose & shared module to separate chunks
-            _options.preserveEntrySignatures = 'strict';
-            if (typeof _options.input === 'string') {
-                _options.input = [_options.input];
-            }
-            Object.keys(provideExposes).forEach((id) => {
-                if (Array.isArray(_options.input)) {
-                    // @ts-ignore
-                    _options.input.push(provideExposes[id]);
-                }
-            });
-            return _options;
-        },
-
-        buildStart(_options: InputOptions) {
-            // @ts-ignore
-            this.emitFile({
-                fileName: options.filename,
-                type: 'chunk',
-                id: remoteEntryHelperId,
-                preserveSignature: "strict"
-            })
-        },
-
-        resolveId(source: string, importer: string) {
-            if (source === remoteEntryHelperId)
-                return source
-            return null;
-        },
-
-        load(id: string) {
-            if (id === remoteEntryHelperId) {
-                return {
-                    code,
-                    moduleSideEffects: "no-treeshake"
-                }
-            }
-            return null
-        },
-
-        generateBundle(_options: OutputOptions, bundle: OutputBundle) {
-            let remoteChunk: OutputChunk;
-            for (const file in bundle) {
-                const chunk = bundle[file];
-                if (chunk.type === 'chunk' && chunk.isEntry) {
-                    for (const key in provideExposes) {
-                        let provideExpose: string = provideExposes[key];
-                        if (chunk.facadeModuleId!.indexOf(path.resolve(provideExpose)) >= 0) {
-                            replaceMap.set(modulePrefix + '${' + provideExpose + '}', `http://localhost:8081/${chunk.fileName}`)
-                        }
-                        if (options.filename === chunk.fileName) {
-                            remoteChunk = chunk;
-                        }
-                    }
-                }
-            }
-            replaceMap.forEach((value, key) => {
-                remoteChunk.code = (remoteChunk.code as string).replace(key, value);
-            })
+    options(_options: InputOptions) {
+      // Split expose & shared module to separate chunks
+      _options.preserveEntrySignatures = 'strict'
+      if (typeof _options.input === 'string') {
+        _options.input = [_options.input]
+      }
+      Object.keys(provideExposes).forEach(id => {
+        if (Array.isArray(_options.input)) {
+          // @ts-ignore
+          _options.input.push(provideExposes[id])
         }
+      })
+      // suppress import warning
+      if (_options.external && !Array.isArray(_options.external)) {
+        _options.external = [_options.external as string]
+      }
+      moduleNames.forEach(item => (_options.external as string[]).push(item))
+      return _options
+    },
+
+    buildStart(_options: InputOptions) {
+      // @ts-ignore
+      this.emitFile({
+        fileName: options.filename,
+        type: 'chunk',
+        id: remoteEntryHelperId,
+        preserveSignature: 'strict'
+      })
+    },
+
+    resolveId(source: string, importer: string) {
+      if (source === remoteEntryHelperId) return source
+      return null
+    },
+
+    load(id: string) {
+      if (id === remoteEntryHelperId) {
+        return {
+          code,
+          moduleSideEffects: 'no-treeshake'
+        }
+      }
+      return null
+    },
+
+    generateBundle(_options: OutputOptions, bundle: OutputBundle) {
+      let remoteChunk: OutputChunk
+      for (const file in bundle) {
+        const chunk = bundle[file]
+        if (chunk.type === 'chunk' && chunk.isEntry) {
+          for (const key in provideExposes) {
+            let provideExpose: string = provideExposes[key]
+            if (
+              chunk.facadeModuleId!.indexOf(path.resolve(provideExpose)) >= 0
+            ) {
+              replaceMap.set(
+                modulePrefix + '${' + provideExpose + '}',
+                `http://localhost:8081/${chunk.fileName}`
+              )
+            }
+            if (options.filename === chunk.fileName) {
+              remoteChunk = chunk
+            }
+          }
+        }
+      }
+      replaceMap.forEach((value, key) => {
+        remoteChunk.code = (remoteChunk.code as string).replace(key, value)
+      })
     }
+  }
 }
