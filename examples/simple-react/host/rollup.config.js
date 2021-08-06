9ccd21549bc9e98b389627ba49d6ffebed61de31
ruleeeer <me@ruleeeer.cn>
2021-08-06 09:52:57 +0800

    test: add react example project

---
+++ examples/simple-react/host/rollup.config.js
@@ -0,0 +1,45 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import injectProcessEnv from 'rollup-plugin-inject-process-env'
+import federation from '@module-federation/rollup-federation'
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.js',
+  preserveEntrySignatures: false,
+  plugins: [
+    injectProcessEnv({
+      NODE_ENV: 'production'
+    }),
+    resolve({
+      browser: true,
+      transformMixedEsModules: true,
+      modulesOnly: true,
+      dedupe: ['react', 'react-dom'],
+      extensions: ['.mjs', '.js', '.jsx', '.json'],
+      preferBuiltins: false
+    }),
+    commonjs({
+      transformMixedEsModules: true,
+      include: ['node_modules/*']
+    }),
+    babel(),
+    federation({
+      remotes: {
+        remote_app: 'http://localhost:8081/remoteEntry.js'
+      },
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: pkg.dependencies.react
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: pkg.dependencies['react-dom']
+        }
+      }
+    })
+  ],
+  output: [{ format: 'system', dir: pkg.main }]
+}
