cc1c3253b5acc0efacb6d33e9ac7605eba21dbdc
ruleeeer <me@ruleeeer.cn>
2021-08-07 15:47:58 +0800

    fix: modify configuration to adapt shared

--- examples/simple-react/remote/rollup.config.js
+++ examples/simple-react/remote/rollup.config.js
@@ -1,47 +1,46 @@
-import babel from 'rollup-plugin-babel'
+import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import injectProcessEnv from 'rollup-plugin-inject-process-env'
 import federation from '@originjs/vite-plugin-federation'
+import replace from '@rollup/plugin-replace'
 import pkg from './package.json'
 
 export default {
   input: 'src/index.js',
-  preserveEntrySignatures: false,
   plugins: [
     // injectProcessEnv({
     //   NODE_ENV: 'production'
     // }),
-    resolve({
-      browser: true,
-      transformMixedEsModules: true,
-      modulesOnly: true,
-      dedupe: ['react', 'react-dom'],
-      extensions: ['.mjs', '.js', '.jsx', 'json'],
-      preferBuiltins: false
-    }),
-    commonjs({
-      transformMixedEsModules: true,
-      include: ['node_modules/*']
-    }),
+    resolve(),
     babel(),
+    commonjs(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      preventAssignment: true
+    }),
     federation({
       filename: 'remoteEntry.js',
       exposes: {
         './Button': 'src/button',
         './Header': 'src/header'
       },
-      shared: {
-        react: {
-          singleton: true,
-          requiredVersion: pkg.dependencies.react
-        },
-        'react-dom': {
-          singleton: true,
-          requiredVersion: pkg.dependencies['react-dom']
+      shared: [
+        {
+          react: {
+            singleton: true,
+            requiredVersion: pkg.dependencies.react
+          },
+          'react-dom': {
+            singleton: true,
+            requiredVersion: pkg.dependencies['react-dom']
+          }
         }
-      }
+      ]
     })
   ],
-  output: [{ format: 'system', dir: pkg.main }]
+  output: {
+    format: 'system',
+    dir: pkg.main
+  },
+  external: ['react', 'react-dom']
 }
