2e06d166c0c222d7f68ffdfc0530499bd7f20f65
Adolph-WSY <adolphwsy@zohomail.com>
2021-09-02 19:22:42 +0800

    feat: Supplementary style file scenarios

--- packages/examples/vue2-demo/remote-simple/src/components/Section.vue
+++ packages/examples/vue2-demo/remote-simple/src/components/Section.vue
@@ -1,22 +1,20 @@
 <template>
   <section :class="$style.section">
-    <h1>
-      This is a section from /remote-simple.
-    </h1>
+    <h1 :class="$style.h1">This is a section from /remote-simple.</h1>
 
     <slot />
   </section>
 </template>
 
 <script>
+import styleSection from './Section.module.css'
+import styleCommon from './Common.module.css'
 export default {
   name: 'Section',
+  computed: {
+    $style() {
+      return Object.assign(styleSection, styleCommon)
+    }
+  }
 }
 </script>
-
-<style module>
-.section {
-  border: 1px solid black;
-  padding: 10px;
-}
-</style>
