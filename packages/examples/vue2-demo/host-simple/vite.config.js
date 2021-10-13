54d47b2336cdc15ba7bef76f35a112d7202b38fe
Adolph-WSY <adolphwsy@zohomail.com>
2021-10-13 14:57:47 +0800

    Update vite.config.js
    optimizeDeps.exclude is automatically configured into the Federation Plugin.

--- packages/examples/vue2-demo/host-simple/vite.config.js
+++ packages/examples/vue2-demo/host-simple/vite.config.js
@@ -15,17 +15,6 @@ export default defineConfig({
       shared: ['vue']
     })
   ],
-  // Dependencies that are forcibly excluded in a pre-build
-  optimizeDeps: {
-    // ...
-    // 抓取预构建的依赖项的入口点，指定自定义条目——该值需要遵循 fast-glob 模式 ，或者是相对于 vite 项目根的模式数组。这将覆盖掉默认条目推断。
-    // 无法抓取远程入口
-    // entries: ['index.html','http://localhost:5011/remoteEntry.js'],
-    // exclude: ['remote-simple'],
-    // exclude: ['remote-simple/*'],
-    // exclude: ['remote-simple', 'remote-simple/remote-simple-button','remote-simple/remote-simple-section'],
-    exclude: ['exclude-simple']
-  },
   server: {force: true},
   build: {
     target: 'es2020',
