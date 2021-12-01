96357085e5945557e6f02789913591995a0590c2
ruleeeer <me@ruleeeer.cn>
2021-12-01 11:19:56 +0800

    test: add vue3-demo-webpack project

---
+++ packages/examples/vue3-demo-webpack/home/src/components/Content.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="webpack-content" style="color: red;">{{ title }}</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: "Webpack Content",
+    };
+  },
+};
+</script>
---
+++ packages/examples/vue3-demo-webpack/layout/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import federation from '@originjs/vite-plugin-federation'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  cacheDir: 'node_modules/.cacheDir',
+  plugins: [
+    vue(),
+    federation({
+      name: 'layout',
+      filename: 'remoteEntry.js',
+      remotes: {
+        home: 'http://localhost:5001/remoteEntry.js'
+      },
+      shared: ['vue', 'vuex']
+    })
+  ],
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        format: 'system',
+        entryFileNames: 'assets/[name].js',
+        minifyInternalExports: false
+      }
+    }
+  }
+})
