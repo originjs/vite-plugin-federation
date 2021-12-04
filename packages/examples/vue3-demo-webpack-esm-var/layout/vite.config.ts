a40cc6e83a9ff0a2803973ca2420fa868355f433
ruleeeer <me@ruleeeer.cn>
2021-12-04 14:42:55 +0800

    test: add webpack var format component project

---
+++ packages/examples/vue3-demo-webpack-esm-var/layout/vite.config.ts
@@ -0,0 +1,34 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import federation from '@originjs/vite-plugin-federation'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    cacheDir: 'node_modules/.cacheDir',
+    plugins: [
+        vue(),
+        federation({
+            name: 'layout',
+            filename: 'remoteEntry.js',
+            remotes: {
+                home: {
+                    external: 'http://localhost:5001/remoteEntry.js',
+                    format: 'var'
+                }
+            },
+            shared: ['vue', 'vuex']
+        })
+    ],
+    build: {
+        target: 'esnext',
+        minify: false,
+        cssCodeSplit: true,
+        rollupOptions: {
+            output: {
+                format: 'esm',
+                entryFileNames: 'assets/[name].js',
+                minifyInternalExports: false
+            }
+        }
+    }
+})
---
+++ packages/examples/vue3-demo-webpack-systemjs/__tests__/vue3-demo-webpack.spec.ts
@@ -0,0 +1,20 @@
+
+
+test('should have no 404s', () => {
+  browserLogs.forEach((msg) => {
+    expect(msg).not.toMatch('404')
+  })
+})
+
+test('Webpack Component', async () => {
+  expect(
+    await page.textContent('#btn-primary')
+  ).toBe('Webpack Component')
+})
+
+test('Webpack Content', async () => {
+  expect(
+    await page.textContent('#webpack-content')
+  ).toBe('Webpack Content')
+})
+
