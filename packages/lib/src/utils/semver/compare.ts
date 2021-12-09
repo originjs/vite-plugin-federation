aa95d28869d7aa20f3ff9049846e3bd834c7c3cd
Dương Tấn Huỳnh Phong <alphanolucifer@gmail.com>
2021-12-09 21:34:06 +0700

    fix semver and add pre-release checker

--- packages/lib/src/utils/semver/compare.ts
+++ packages/lib/src/utils/semver/compare.ts
@@ -4,12 +4,12 @@ export interface CompareAtom {
   major: string
   minor: string
   patch: string
-  preRelease?: string
+  preRelease?: string[]
 }
 
 function compareAtom(rangeAtom: string | number, versionAtom: string | number): number {
-  rangeAtom = +rangeAtom
-  versionAtom = +versionAtom
+  rangeAtom = +rangeAtom || rangeAtom
+  versionAtom = +versionAtom || versionAtom
 
   if (rangeAtom > versionAtom) {
     return 1
@@ -22,10 +22,53 @@ function compareAtom(rangeAtom: string | number, versionAtom: string | number): 
   return -1
 }
 
+function comparePreRelease(rangeAtom: CompareAtom, versionAtom: CompareAtom): number {
+  const { preRelease: rangePreRelease } = rangeAtom
+  const { preRelease: versionPreRelease } = versionAtom
+
+  if (rangePreRelease === undefined && !!versionPreRelease) {
+    return 1
+  }
+
+  if (!!rangePreRelease && versionPreRelease === undefined) {
+    return -1
+  }
+
+  if (rangePreRelease === undefined && versionPreRelease === undefined) {
+    return 0
+  }
+
+  for (let i = 0, n = rangePreRelease!.length; i <= n; i++) {
+    const rangeElement = rangePreRelease![i]
+    const versionElement = versionPreRelease![i]
+
+    if (rangeElement === versionElement) {
+      continue;
+    }
+
+    if (rangeElement === undefined && versionElement === undefined) {
+      return 0
+    }
+
+    if (!rangeElement) {
+      return 1
+    }
+
+    if (!versionElement) {
+      return -1
+    }
+
+    return compareAtom(rangeElement, versionElement)
+  }
+
+  return 0
+}
+
 function compareVersion(rangeAtom: CompareAtom, versionAtom: CompareAtom): number {
   return compareAtom(rangeAtom.major, versionAtom.major)
     || compareAtom(rangeAtom.minor, versionAtom.minor)
     || compareAtom(rangeAtom.patch, versionAtom.patch)
+    || comparePreRelease(rangeAtom, versionAtom)
 }
 
 function eq(rangeAtom: CompareAtom, versionAtom: CompareAtom): boolean {
@@ -45,6 +88,9 @@ export function compare(rangeAtom: CompareAtom, versionAtom: CompareAtom): boole
       return compareVersion(rangeAtom, versionAtom) > 0
     case '<=':
       return eq(rangeAtom, versionAtom) || compareVersion(rangeAtom, versionAtom) > 0
+    case undefined: { // mean * or x -> all versions
+      return true
+    }
     default:
       return false
   }
