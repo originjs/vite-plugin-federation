c50a3a695fd1c06124297ce52aa793579dcbaba2
ruleeeer <me@ruleeeer.cn>
2022-04-27 16:38:26 +0800

    test: add AST node type ExportNamedDeclaration code demo

--- packages/examples/vue3-demo-esm/layout/src/store.js
+++ packages/examples/vue3-demo-esm/layout/src/store.js
@@ -1,5 +1,9 @@
 import { createStore } from 'vuex';
 
+
+// This is just to test if the export from remote works, there is no real point
+export {name} from "home/Content";
+
 export default createStore({
     state() {
         return {
