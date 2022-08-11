8cc492c6fcc2094b7e1f729a0f599a8da4235569
ruleeeer <me@ruleeeer.cn>
2022-08-11 16:09:43 +0800

    feat: add removed top-level-await config

--- packages/examples/vue3-demo-esm/common-lib/vite.config.ts
+++ packages/examples/vue3-demo-esm/common-lib/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import federation from '@originjs/vite-plugin-federation'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,11 +18,16 @@ export default defineConfig({
         vue: {
           requiredVersion: '^3.0.0'
         }
-      }
+      },
+    }),
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: "__tla",
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: i => `__tla_${i}`
     })
   ],
   build: {
-    target: 'esnext',
     minify: false,
     cssCodeSplit: true,
     rollupOptions: {
