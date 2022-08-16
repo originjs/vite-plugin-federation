5fb0a31f7ff0e09e17f31fd0d3621db5aa9e1226
Joe Zhang <flyfish.zy@gmail.com>
2022-08-16 09:24:01 +0800

    feat: support rollup v3.0 (#230)

--- packages/examples/vitestGlobalSetup.ts
+++ packages/examples/vitestGlobalSetup.ts
@@ -46,7 +46,7 @@ export async function teardown(): Promise<void> {
   browserServer?.close()
   if (!process.env.VITE_PRESERVE_BUILD_ARTIFACTS) {
     await kill('5000,5001,5002,5003')
-    await fs.remove(path.resolve(__dirname, '../packages/temp'), (err) => {
+    await fs.remove(path.resolve(__dirname, '../temp'), (err) => {
       if (err) return console.log(err)
       console.log('temp file is deleted')
     })
