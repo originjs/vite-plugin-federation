6aa25caa4dd350bdf8518d85965c4714ddb2a70f
Dmitrii Derepko <xepozz@list.ru>
2023-07-03 17:12:01 +0300

    Add new example

---
+++ packages/examples/react-vite-dynamic/host/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import federation from '@originjs/vite-plugin-federation'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'app',
+      remotes: {},
+      shared: ['react','react-dom']
+    })
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
+})
